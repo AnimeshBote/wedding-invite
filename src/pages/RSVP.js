088cdd9e6dc3b5d96c8fa7ff737a5e2e18d0a6e6
AnimeshBote <boteanimesh97@gmail.com>
2024-12-07 12:25:13 -0500

    updated RSVP form styling

--- src/pages/RSVP.js
+++ src/pages/RSVP.js
@@ -1,70 +1,18 @@
-import { useNavigate } from 'react-router-dom';
 import './Rsvp.css';
 const RSVP = () => {
-  const navigate = useNavigate();
-  // const { register, handleSubmit } = useForm();
-  const handleSubmit = event => {
-    event.preventDefault();
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString()
-    })
-      .then(() => navigate("/thank-you/"))
-      .catch(error => alert(error));
-  };
 
-
-  // return (
-  //   <div className="rsvp-container">
-  //     <h2 className="rsvp-title">RSVP to Our Wedding</h2>
-  //     <form name="rsvp" method="post" data-netlify="true" className="rsvp-form">
-  //     <input type="hidden" name="form-name" value="rsvp" />
-  //       <div className="form-group">
-  //         <label htmlFor="name" className="form-label">Your Name</label>
-  //         <input
-  //           id="name"
-  //           type="text"
-  //           {...register('name')}
-  //           placeholder="Enter your name"
-  //           required
-  //           className="form-input"
-  //         />
-  //       </div>
-  //       <div className="form-group">
-  //         <label htmlFor="email" className="form-label">Your Email</label>
-  //         <input
-  //           id="email"
-  //           type="email"
-  //           {...register('email')}
-  //           placeholder="Enter your email"
-  //           required
-  //           className="form-input"
-  //         />
-  //       </div>
-  //       <button type="submit" className="rsvp-submit-btn">Submit</button>
-  //     </form>
-  //   </div>
-  // );
 
   return (
-    <form name="contact" method="post">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label>Your Name: <input type="text" name="name" /></label>
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <div class="container">
+      <form name="contact" method="post">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>Welcome</p>
+        <input type="text" placeholder="Name" /><br />
+        <input type="date" placeholder="Arrival Date" /><br />
+        <input type="text" placeholder="Guest Count" /><br />
+        <input type="submit" value="Send" /><br />
+      </form>
+    </div>
   );
 
 };
